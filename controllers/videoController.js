87ce12a3b797f4d856afc0903c9c09a291addfe0
SugarGW <kowonp@gmail.com>
2021-03-22 18:59:04 +0900

    #2.13~16

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,9 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("Search");
+// Global Controller
+export const home = (req, res) => res.render("home");
+export const search = (req, res) => res.render("search");
 
-export const videos = (req, res) => res.send("videos");
-export const upload = (req, res) => res.send("Upload");
-export const videoDetail = (req, res) => res.send("videoDetail");
-export const editVideo = (req, res) => res.send("editVideo");
-export const deleteVideo = (req, res) => res.send("deleteVideo");
+// Video Controller
+export const upload = (req, res) => res.render("upload");
+export const videoDetail = (req, res) => res.render("videoDetail");
+export const editVideo = (req, res) => res.render("editVideo");
+export const deleteVideo = (req, res) => res.render("deleteVideo");
--- init.js
+++ init.js
@@ -3,7 +3,7 @@ import app from "./app";
 const PORT = 5000;
 
 const handleListening = (req, res) => {
-  console.log(`🌳 http://localhost:${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 };
 
 app.listen(PORT, handleListening);
